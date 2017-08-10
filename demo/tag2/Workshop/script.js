b2ee60631918e907a35bf0f0125daac7f659e78a
Lena Winter <lena.k.winter@gmail.com>
2017-08-10 15:29:30 +0200

    End of day 2

--- demo/tag2/Workshop/script.js
+++ demo/tag2/Workshop/script.js
@@ -14,10 +14,34 @@ function aendereHintergrund(){
 	if(wahl === "blumen"){
 		document.querySelector("body").style.backgroundImage = "url(bilder/hintergrund/blumen.png)";
 		document.querySelector("body").style.backgroundSize = "auto";
+	}else if(wahl === "zitronen"){
+		document.querySelector("body").style.backgroundImage = "url(bilder/hintergrund/zitronen.png)";
+		document.querySelector("body").style.backgroundSize = "auto";
 	}else{
 		document.querySelector("body").style.backgroundImage = "url(bilder/hintergrund/pink-blau.png)";
 		document.querySelector("body").style.backgroundSize = "cover";
 	}
+}
+
+function schleife(){
+	var limit = prompt("Gebe das Limit an", "");
+	var summe = 0;
 	
+	for(var zaehler = 1; zaehler <= limit; zaehler =  zaehler + 1){
+		summe = summe + zaehler;
+	}
+	
+	document.getElementById("ergebnis").textContent = summe;
+
+}
+
+
+
+function fruechte(){
+	var obst = ["apfel", "birne", "banane"]
+	
+	var wahl = prompt("Wähle ein Obst aus", "Gebe eine Zahl von 0 bis 2 an");
+	
+	document.getElementById("obstsalat").textContent = obst[wahl];
 
 }
